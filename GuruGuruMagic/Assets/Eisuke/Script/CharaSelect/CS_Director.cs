d9cf5c3fa88bcf2ccc29fd1705b12a624639fce1
KBC11605156\kbc11605156 <eisuke-t-16d@kbc.ac.jp>
2018-09-07 10:11:31 +0900

    タイトル・ホーム・キャラ選択・ステージ選択(仮)

---
+++ GuruGuruMagic/Assets/Eisuke/Script/CharaSelect/CS_Director.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CS_Director : MonoBehaviour {
+    enum SWIPE { NONE, LEFT, RIGHT };
+    SWIPE s_type;
+    public GameObject center;
+    Vector2 spos = new Vector2(0, 0);
+    Vector2 epos = new Vector2(0, 0);
+    Vector3 dpos;
+    float cnt;
+    const float cnt_max = 1.0f;
+    int c_ix;
+    const int ix_max = 4;
+    Vector3[] Chara_Index = { 
+                                new Vector3(  0,0,0),
+                                new Vector3( -5,0,0),
+                                new Vector3(-10,0,0),
+                                new Vector3(-15,0,0),
+                            };
+	void Start () {
+        QualitySettings.vSyncCount = 0;     //VSyncをOFFにする
+        Application.targetFrameRate = 60;   //ターゲットフレームレート
+        cnt = 0;
+        c_ix = 0;
+        dpos = spos - epos;
+	}
+	
+	void Update () {
+        if (s_type != SWIPE.NONE) Slide_Sprite();
+        else KeyGet(Input.mousePosition);
+	}
+    void Slide_Sprite() {
+        center.transform.position += dpos * 5.0f * Time.deltaTime;
+        cnt += Time.deltaTime;
+        dpos = (Chara_Index[c_ix] - center.transform.position) * cnt * 1.25f;
+        if (cnt >= cnt_max){
+            center.transform.position = Chara_Index[c_ix];
+            cnt = 0;
+            s_type = SWIPE.NONE;
+            
+        }
+    }
+    void KeyGet(Vector2 mpos) {
+        
+        if (Input.GetMouseButtonDown(0)){
+            spos = new Vector2(mpos.x, mpos.y);
+            cnt = 0;
+        }
+        if (Input.GetMouseButtonUp(0)){
+            epos = new Vector2(mpos.x, mpos.y);
+            GetDirection(spos, epos);
+        }
+    }
+    void GetDirection(Vector2 s,Vector2 e) {
+        float dirX = e.x - s.x;
+        float dirY = e.y - s.y;
+        string dir = "none";
+        if (Mathf.Abs(dirY) < Mathf.Abs(dirX)){
+            if (30 < dirX) dir = "right";
+            if (-30 > dirX) dir = "left";
+        }else dir = "touch";
+        switch (dir) {
+            case "right":
+                s_type = SWIPE.RIGHT;
+                c_ix--;
+                break;
+            case "left":
+                s_type = SWIPE.LEFT;
+                c_ix++;
+                break;
+        }
+        if (c_ix >= ix_max) c_ix = ix_max - 1;
+        else if (c_ix < 0) c_ix = 0;
+    }
+    public void Select_Chara() {
+        PlayerPrefs.SetInt("SelectCharactor", c_ix);
+        SceneManager.LoadScene("HomeScene");
+    }
+    public void HomeBack() {
+        SceneManager.LoadScene("HomeScene");
+    }
+}
---
+++ GuruGuruMagic/Assets/Eisuke/Script/QuestSelect/QS_Director.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QS_Director : MonoBehaviour {
+    enum SWIPE { NONE, LEFT, RIGHT };
+    SWIPE s_type;
+    public GameObject Center;
+    const int child_max = 6;
+    GameObject[] CenterChild = new GameObject[child_max];
+    Vector2 Spos = new Vector2(0, 0);
+    Vector2 Epos = new Vector2(0, 0);
+    float cnt;
+    const float cnt_max = 1.0f;
+    int q_ix,old_ix;
+    const int ix_max = 6;
+    Vector3[] Quest_Index = {
+                                new Vector3(0,0,0),
+                                new Vector3(0,0,60),
+                                new Vector3(0,0,120),
+                                new Vector3(0,0,180),
+                                new Vector3(0,0,240),
+                                new Vector3(0,0,300),
+                            };
+    void Start () {
+        QualitySettings.vSyncCount = 0;     //VSyncをOFFにする
+        Application.targetFrameRate = 60;   //ターゲットフレームレート
+        cnt = 0;
+        q_ix = old_ix = 0;
+        int c_cnt = Center.transform.childCount;
+        for (int i = 0; i < c_cnt; i++) {
+            CenterChild[i] = Center.transform.GetChild(i).gameObject;    
+        }
+	}   
+	void Update () {
+        if (s_type != SWIPE.NONE) Roll_Sprite();
+        else KeyGet(Input.mousePosition);
+	}
+    void Roll_Sprite() {
+        if (cnt <= cnt_max) {
+            cnt += Time.deltaTime;
+            Quaternion q1 = Quaternion.Euler(Quest_Index[old_ix]);
+            Quaternion q2 = Quaternion.Euler(Quest_Index[q_ix]);
+            Center.transform.rotation = Quaternion.Lerp(q1, q2, cnt);
+            StageScale(q_ix, cnt);
+        }else{
+            s_type = SWIPE.NONE;
+            cnt = 0;
+        }
+    }
+    void StageScale(int ix, float scale) {
+        scale = 1 + (scale * 0.5f);
+        CenterChild[ix].transform.localScale = new Vector3(scale, scale, 1);
+        bool[] check = new bool[child_max];
+        for (int i = 0; i < child_max; i++) check[i] = false;
+        for (int i = 0; i < child_max; i++) {
+            if (i != ix) check[i] = true;
+            if (check[i]) CenterChild[i].transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+    void KeyGet(Vector2 mpos) {
+        if (Input.GetMouseButtonDown(0)) {
+            Spos = new Vector2(mpos.x, mpos.y);
+            cnt = 0;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            Epos = new Vector2(mpos.x, mpos.y);
+            GetDirction(Spos,Epos);
+        }
+    }
+    void GetDirction(Vector2 s, Vector2 e) {
+        float dirX = e.x - s.x;
+        float dirY = e.y - s.y;
+        string dir = "none";
+        s_type = SWIPE.NONE;
+        if (Mathf.Abs(dirY) < Mathf.Abs(dirX)){
+            if (30 < dirX) dir = "right";
+            if (-30 > dirX) dir = "left";
+        }else if (Mathf.Abs(dirY) > Mathf.Abs(dirX)){
+            if (30 < dirY) dir = "up";
+            if (-30 > dirY) dir = "down";
+        }else dir = "touch";
+        switch (dir){
+            case "right":
+                s_type = SWIPE.RIGHT;
+                old_ix = q_ix;
+                q_ix--;
+                break;
+            case "left":
+                s_type = SWIPE.LEFT;
+                old_ix = q_ix;
+                q_ix++;
+                break;
+        }
+        if (q_ix >= ix_max) q_ix = 0;
+        if (q_ix < 0) q_ix = ix_max - 1;
+    }
+    public void QS_Button() {
+        Debug.Log("q_ix" + q_ix);
+    }
+}
